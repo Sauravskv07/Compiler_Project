69d140a88e0b2c2c147a28d3323724464ff3f926
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2020-02-21 01:35:50 +0530

    Refractoring

---
+++ app/driver.c
@@ -0,0 +1,97 @@
+#include "lexer.h"
+#include "hash_table.h"
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Start..");
+
+	mapping_table= ht_new();
+	
+	keyword_table= ht_new();
+
+	int i=0;
+	char line[40];
+	size_t len = 0;
+	ssize_t read;
+
+	FILE* keyword_file=fopen("keywords.txt","r");	
+
+    	if (keyword_file == NULL)
+	{
+		printf("UNABLE TO OPEN FILE KEYWORDS.TXT");
+        	exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, 40, keyword_file)!=NULL) {
+        	line[strcspn(line, "\n")] = 0;
+		ht_insert(keyword_table,line, i++,KEYWORD);
+        	//printf("%s", line);
+    	}
+
+	fclose(keyword_file);
+
+	i=0;
+
+	FILE* terminal_file=fopen("terminals.txt","r");
+	
+    	if (terminal_file == NULL)
+	{
+		printf("UNABLE TO OPEN FILE TERMINALS.TXT");
+        	exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, 40, terminal_file)!=NULL) {
+        	line[strcspn(line, "\n")] = 0;
+		terms[i] = line;
+		ht_insert(mapping_table,line, i++,TERMINAL);
+        	//printf("%s", line);
+    	}
+
+	fclose(terminal_file);
+
+	FILE* nonterminal_file=fopen("nonterminals.txt","r");
+	
+    	if (nonterminal_file == NULL)
+	{
+		printf("UNABLE TO OPEN FILE NONTERMINALS.TXT");
+        	exit(EXIT_FAILURE);
+	}
+	while (fgets(line, 40, nonterminal_file)!=NULL) {
+        	line[strcspn(line, "\n")] = 0;
+		terms[i] = line;
+		ht_insert(mapping_table,line, i++,NONTERMINAL);
+        	//printf("%s", line);
+    	}
+
+	fclose(nonterminal_file);
+
+	
+	fp=(FILE*)fopen("program.c","r");
+	
+	if(fp==NULL)
+	{
+		printf("Some error while opening the file");
+		exit(1);
+	}
+
+	fp=getStream(fp);
+	
+	forwardPointer=-1;
+
+	Token* next=NULL;
+
+	while(1)
+	{	
+		next=getNextToken();
+		if(!next)
+			break;
+		
+		printf("LEXEME=  %s  VALUE=  %d  LINE NUMBER=  %d  TYPE=  %d\n",next->lexeme,next->val,next->LN,next->index);
+	}
+
+	printf("Lexical Analysis Successful\n");
+	//fclose(fp);
+	exit(0);
+}
---
+++ app/lexer.c
@@ -0,0 +1,476 @@
+#include "lexer.h"
+#include "hash_table.h"
+
+int j_pointer=0;
+int state=1;
+int LN=1;
+int backPointer=0;
+int forwardPointer=0;
+
+FILE * getStream(FILE* fp)
+{
+	int i=0;
+	for(;i<(backPointer-forwardPointer);i++)
+	{
+		buffer[i]=buffer[backPointer+i];
+		//printf("%d  %c \n",i,buffer[i]);
+	}
+
+	for(;i<MAX_BUFF_SIZE;i++)
+	{
+		buffer[i]=getc(fp);
+		//printf("%d %c\n",i,buffer[i]);
+		if(buffer[i]==EOF)
+		{
+			buffer[i]='\0';
+			//printf("Reached End of the Program\n");
+			break;
+		}
+		//printf("%d  %c \n",i,buffer[i]);
+	}	
+}
+
+Token* createToken(int index,int bp,int fp, int ln)
+{
+	Token* temp=(Token*) malloc(sizeof(Token));
+	
+	//printf("backpointer= %d forwardPointer= %d",bp,fp);
+	int i=bp;	
+
+	for(;i<=fp;i++)
+		temp->lexeme[i-bp]=buffer[i];
+	temp->lexeme[i]='\0';
+
+	temp->index=index;
+	temp->LN=ln;
+	temp->val.i_val=0;
+
+	if(index==ht_search(mapping_table,"ID")->index)
+	{
+		if(ht_search(keyword_table,temp->lexeme))
+		{	
+			j_pointer=0;
+			while(temp->lexeme[j_pointer])
+			{
+				token[j_pointer]=toupper(temp->lexeme[j_pointer++]);
+			}
+			token[j_pointer]=0;			
+
+			temp->index=ht_search(mapping_table,token)->index;	
+		}
+		else
+		{
+			if(strlen(temp->lexeme)>20)
+				temp->index=-1;
+		}
+	}
+
+	if(index==ht_search(mapping_table,"NUM")->index)
+	{	
+	
+	}
+
+	else if(index==ht_search(mapping_table,"RNUM")->index)
+	{}
+
+	return temp;
+}
+
+Token* getNextToken()
+{
+	state=1;
+
+	while(1)
+	{
+		forwardPointer++;
+	
+		if(forwardPointer==MAX_BUFF_SIZE)
+		{	
+			fp=getStream(fp);
+		}
+		//printf("forward pointer = %d\n",forwardPointer);
+		character_read=buffer[forwardPointer];
+
+		//printf("State = %d  Character Read = %c\n",state,character_read);
+				
+		switch(state)
+		{
+			case 1:
+			{
+				backPointer=forwardPointer;
+				
+				if(character_read==0)
+				{
+					return NULL;
+				}
+				else if(character_read=='(')
+				{
+					return createToken(ht_search(mapping_table,"BO")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read==')')
+				{
+					return createToken(ht_search(mapping_table,"BC")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read=='+')
+				{
+					return createToken(ht_search(mapping_table,"PLUS")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read=='-')
+				{
+					return createToken(ht_search(mapping_table,"MINUS")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read=='[')
+				{
+					return createToken(ht_search(mapping_table,"SQBO")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read==']')
+				{
+					return createToken(ht_search(mapping_table,"SQBC")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read=='\n')
+				{
+					state=1;
+					LN++;
+				}
+				else if(character_read==':')
+				{
+					state=35;
+				}
+				else if(character_read=='/')
+				{
+					return createToken(ht_search(mapping_table,"DIV")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read==';')
+				{
+					return createToken(ht_search(mapping_table,"SEMICOL")->index,backPointer,forwardPointer,LN);
+				}
+				else if(character_read=='<')
+				{
+					state=38;
+				}
+				else if(character_read=='>')
+				{
+					state=42;
+				}
+				else if(character_read=='.')
+				{
+					state=47;
+				}
+				else if(character_read==' ' || character_read=='\t')
+				{
+					state=31;
+				}
+				else if(character_read=='=')
+				{
+					state=21;
+				}				
+				else if(character_read=='!')
+				{
+					state=18;
+				}
+
+				else if(('a'<=character_read && character_read<='z') || ('A'<=character_read && character_read<='Z'))
+				{
+					state=15;
+				}
+				else if(character_read=='*')
+				{
+					state=24;
+				}
+				else if('0'<=character_read && character_read<='9')
+				{
+					state=2;
+				}
+				break;
+					
+			}
+			case 35:
+			{
+				if(character_read=='=')
+				{
+					return createToken(ht_search(mapping_table,"ASSIGNOP")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"COLON")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 38:
+			{
+				if(character_read=='<')
+				{
+					state=43;
+				}
+				else if(character_read=='=')
+				{
+					return createToken(ht_search(mapping_table,"LE")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{	
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"LT")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 43:
+			{
+
+				if(character_read=='<')
+				{
+					return createToken(ht_search(mapping_table,"DRIVERDEF")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"DEF")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 42:
+			{
+				if(character_read=='>')
+				{
+					state=46;
+				}
+				else if(character_read=='=')
+				{
+					return createToken(ht_search(mapping_table,"GE")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"GT")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 46:
+			{
+
+				if(character_read=='>')
+				{
+					return createToken(ht_search(mapping_table,"ENDDRIVERDEF")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"ENDDEF")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 47:
+			{
+				if(character_read=='.')
+				{
+					return createToken(ht_search(mapping_table,"RANGEOP")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(-1,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+
+			case 31:
+			{
+				if(character_read==' ' ||  character_read=='\t')
+				{
+					state=31;
+				}
+				else
+				{
+					state=1;
+					forwardPointer--;
+				}
+				break;
+			}
+
+			case 21:
+			{
+				if(character_read=='=')
+				{
+					return createToken(ht_search(mapping_table,"EQ")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(-1,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 18:
+			{
+				if(character_read=='=')
+				{
+					return createToken(ht_search(mapping_table,"NE")->index,backPointer,forwardPointer,LN);
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(-1,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+
+			case 15:
+			{
+				//printf("%c\n",character_read);
+				if(character_read=='_' ||('a'<=character_read && character_read<='z') || ('A'<=character_read && character_read<='Z')|| ('0'<= character_read && character_read<='9'))
+				{
+					state=15;
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"ID")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 24:
+			{
+				if(character_read=='*')
+				{
+					state=26;
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"MUL")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}	
+			case 26:
+			{
+				if(character_read=='*')
+				{
+					state=27;
+				}
+				else
+				{
+					if(character_read=='\n')
+						LN++;
+					state=26;
+				}
+				break;
+			}
+			case 27:
+			{
+				if(character_read=='*')
+				{
+					state=1;
+				}
+				else
+				{
+					if(character_read=='\n')
+						LN++;
+					state=26;
+				}
+
+				break;
+			}
+
+			case 2:
+			{
+				if('0'<= character_read && character_read<='9')
+				{
+					state=2;
+				}
+				else if(character_read=='.')
+				{
+					state=4;
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"NUM")->index,backPointer,forwardPointer,LN);		
+				}
+				break;
+			}
+			case 4:
+			{
+				if(character_read=='.')
+				{
+					forwardPointer-=2;
+					return createToken(ht_search(mapping_table,"NUM")->index,backPointer,forwardPointer,LN);
+				}
+				else if('0'<= character_read && character_read<='9')
+				{
+					state=6;
+				}		
+				else
+				{
+					forwardPointer-=2;
+					return createToken(ht_search(mapping_table,"NUM")->index,backPointer,forwardPointer,LN);
+				}		
+				break;
+			}
+			case 6:
+			{
+				if('0'<= character_read && character_read<='9')
+				{
+					state=6;
+				}
+				else if(character_read=='e' || character_read=='E')
+				{
+					state=8;
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"RNUM")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 8:
+			{
+				if(character_read=='+' || character_read=='-')
+				{
+					state=10;
+				}
+				else if('0'<= character_read && character_read<='9')
+				{
+					state=11;
+				}
+				else
+				{
+					forwardPointer-=2;
+					return createToken(ht_search(mapping_table,"RNUM")->index,backPointer,forwardPointer,LN);
+				}
+				break;
+			}
+			case 10:
+			{
+				if('0'<= character_read && character_read<='9')
+				{
+					state=11;
+				}
+				else
+				{
+					forwardPointer-=3;
+					return createToken(ht_search(mapping_table,"RNUM")->index,backPointer,forwardPointer,LN);
+				}
+				break;			
+			}
+
+			case 11:
+			{
+				if('0'<= character_read && character_read<='9')
+				{
+					state=11;
+				}
+				else
+				{
+					forwardPointer--;
+					return createToken(ht_search(mapping_table,"RNUM")->index,backPointer,forwardPointer,LN);
+				}			
+				break;
+			}
+		}
+	}	
+}
+
+
+
+
